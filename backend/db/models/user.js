3072e3150e691cac2898c96170a43015b65f3387
Elliot <Heecher626@gmail.com>
2023-09-04 14:51:55 -0400

    test

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Membership, {foreignKey: 'userId'})
       User.hasMany(models.Attendance, {foreignKey: 'userId'})
       User.hasMany(models.Group, {foreignKey: 'organizerId'})
-      User.belongsToMany(models.Event, {through: models.Attendance, foreignKey: 'userId', otherKey: 'eventId'})
+      //User.belongsToMany(models.Event, {through: models.Attendance, foreignKey: 'userId', otherKey: 'eventId'})
       User.belongsToMany(models.Group, {through: models.Membership, foreignKey: 'userId', otherKey: 'groupId', as: 'memberships'})
     }
   }
