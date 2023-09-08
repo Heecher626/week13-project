a1d6192e2280d63843065a75b1a6eda6d32e844f
Elliot <Heecher626@gmail.com>
2023-09-08 14:00:27 -0400

    test

--- backend/db/seeders/20230904024400-demo-group.js
+++ backend/db/seeders/20230904024400-demo-group.js
@@ -1,49 +1,49 @@
-'use strict';
+// 'use strict';
 
-const { sequelize } = require('../models');
+// const { sequelize } = require('../models');
 
-let {Group} = require('../models')
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
-}
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-   //options.tableName = 'Groups'
-   await Group.bulkCreate( [{
-    organizerId: 1,
-    name: 'group1',
-    about: 'This is a group',
-    type: 'Online',
-    private: true,
-    city: 'Boston',
-    state: 'Massachusetts',
-   }], {validate: true})
-  },
+// let {Group} = require('../models')
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;
+// }
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//    //options.tableName = 'Groups'
+//    await Group.bulkCreate( [{
+//     organizerId: 1,
+//     name: 'group1',
+//     about: 'This is a group',
+//     type: 'Online',
+//     private: true,
+//     city: 'Boston',
+//     state: 'Massachusetts',
+//    }], {validate: true})
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    options.tableName = 'Groups'
-    const Op = Sequelize.Op
-    await queryInterface.bulkDelete(options, {
-      name: {
-        [Op.in]: ['group1']
-      }
-    }, {})
-  }
-};
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//     options.tableName = 'Groups'
+//     const Op = Sequelize.Op
+//     await queryInterface.bulkDelete(options, {
+//       name: {
+//         [Op.in]: ['group1']
+//       }
+//     }, {})
+//   }
+// };
